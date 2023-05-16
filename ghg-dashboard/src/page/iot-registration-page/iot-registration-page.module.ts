d8c9406c3bfd84195adea8cf95d88b74c45b0df1
zahraariff <fatimazahra0901@gmail.com>
2023-05-16 11:12:24 +0800

    Create iot-registration-page.module.ts
    IoT Registration Page initial setup

---
+++ ghg-dashboard/src/page/iot-registration-page/iot-registration-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IotRegistrationPageComponent } from './iot-registration-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    IotRegistrationPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IotRegistrationPageModule { }
