64eeb21152254b7cdb53017a3df7928ab9e7c3b7
zahraariff <fatimazahra0901@gmail.com>
2023-11-05 15:41:33 +0800

    Modified graph UI and some admin module

--- ghg-dashboard/model/emissionData.model.js
+++ ghg-dashboard/model/emissionData.model.js
@@ -3,19 +3,24 @@ const Schema = mongoose.Schema;
 
 // List of columns for emissionData schema
 
-let emissionData = new Schema({
-    scope: {
-        type: String
-    },
-    dataType: {
-        type: String
-    },
-    dataDesc: {
-        type: String
-    },
-    dataValue: {
-        type: Number
+let emissionData = new Schema(
+    {
+        scope: {
+            type: String
+        },
+        dataType: {
+            type: String
+        },
+        dataDesc: {
+            type: String
+        },
+        dataValue: {
+            type: Number
+        },
+    }, 
+    {
+        timestamps: true // Automatically add createdAt and updatedAt fields
     }
-});
+);
 
 module.exports = mongoose.model('emissionData', emissionData);
