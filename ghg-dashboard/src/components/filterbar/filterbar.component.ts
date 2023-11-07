bbe5202ce8c080837d618aea251d760019bc112c
zahraariff <fatimazahra0901@gmail.com>
2023-11-07 14:48:02 +0800

    Adding FilterBar Component on Graph Page

--- ghg-dashboard/src/components/filterbar/filterbar.component.ts
+++ ghg-dashboard/src/components/filterbar/filterbar.component.ts
@@ -6,11 +6,45 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./filterbar.component.scss']
 })
 export class FilterbarComponent {
+  // Variables to toggle the filterbar in the parent
   @Input() isChildActive: boolean | undefined;
   @Output() isChildActiveChange = new EventEmitter<boolean>();
+
+  // Variables to filter the graphs
+  @Input() selectedScopes: number[] | undefined; // Data from the parent component
+  @Input() selectedEmissionTypes: string[] | undefined; // Data from the parent component
+  @Output() filterScopes = new EventEmitter<number[]>();
+  @Output() filterEmissionTypes = new EventEmitter<string[]>();
 
   toggleChildClass(){
     this.isChildActive = !this.isChildActive;
     this.isChildActiveChange.emit(this.isChildActive);
   }
+
+   applyScopesFilter(scope: number) {
+    console.log('method clicked')
+    if(this.selectedScopes) {
+      if (this.selectedScopes.includes(scope)) {
+        this.selectedScopes = this.selectedScopes.filter((s) => s !== scope);
+      } else {
+        this.selectedScopes.push(scope);
+      }
+      this.filterScopes.emit([scope]); // Emit the filtered data back to the parent
+   
+    }
+    
+  }
+
+  applyEmissionTypesFilter(emissionType: string) {
+    console.log('method clicked')
+    if(this.selectedEmissionTypes){
+      if (this.selectedEmissionTypes.includes(emissionType)) {
+        this.selectedEmissionTypes = this.selectedEmissionTypes.filter((type) => type !== emissionType);
+      } else {
+        this.selectedEmissionTypes.push(emissionType);
+      }
+    }
+    this.filterEmissionTypes.emit([emissionType]); // Emit the filtered data back to the parent
+  }
+
 }
--- ghg-dashboard/src/page/graph-page/graph-page.component.ts
+++ ghg-dashboard/src/page/graph-page/graph-page.component.ts
@@ -10,6 +10,21 @@ export class GraphPageComponent {
 
   filterbarComponent: FilterbarComponent;
   isChildActive: boolean = false;
+  
+  selectedScopes: number[] = []; // Initially no scope filter are selected
+  selectedEmissionTypes: string[] = []; // Initially no emission types filter are selected
+
+  // Define properties to associate graphs with scopes
+  graphScopes: { [key: string]: number } = {
+    graph1: 0,
+    graph2: 0,
+    graph3: 0,
+    graph4: 0,
+    graph5: 1,
+    graph6: 1,
+    graph7: 2,
+    graph8: 3
+  };
 
   constructor() {
     this.filterbarComponent = new FilterbarComponent();
@@ -28,5 +43,25 @@ export class GraphPageComponent {
  toggleChildClass() {
   this.isChildActive = !this.isChildActive;
  }
+
+ filterGraphs(selectedScopes: number[]) { // this method's argument is data emitted from the child
+  this.selectedScopes = selectedScopes;
+}
+
+filterEmissionDataTypes(selectedEmissionTypes: string[]) { // this method's argument is data emitted from the child
+  this.selectedEmissionTypes = selectedEmissionTypes;
+  }
+
+shouldDisplayGraph(scope: number, emissionType: string): boolean {
+  if (this.selectedScopes.length === 0 && this.selectedEmissionTypes.length === 0) {
+    return true; // Show all graphs when no filters are selected
+  }
+
+  const scopeMatches = this.selectedScopes.length === 0 || this.selectedScopes.includes(scope);
+  const typeMatches = this.selectedEmissionTypes.length === 0 || this.selectedEmissionTypes.includes(emissionType);
+
+  return scopeMatches && typeMatches;
+}
+
  
 }
